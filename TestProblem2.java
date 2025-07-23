b205a6e3e477e5285fdc5d253e13fa916815d4c9
Rohan Nandan <rohannandan2004@gmail.com>
2025-07-23 13:34:49 +1200

    Feat: OOP - Inheritance + Interface

---
+++ TestProblem2.java
@@ -0,0 +1,56 @@
+interface Shape{
+    abstract public double calcArea();
+}
+
+class Point implements Shape{
+    private int x;
+    private int y;
+
+    public Point(){}
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString(){
+        return "x = " + x + ", y = " + y;
+    }
+
+    @Override
+    public double calcArea() {
+        return 0.0;
+    }
+}
+
+class Circle extends Point{
+    private Point origin;
+    private int radius;
+
+    public Circle(Point p, int radius){
+        this.origin = p;
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString(){
+        return "Circle" + origin.toString() + ", radius = " + this.radius;
+    }
+
+    @Override
+    public double calcArea(){
+        return Math.PI*this.radius * this.radius;
+    }
+}
+
+public class TestProblem2{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        Point p = new Point(2,3);
+        System.out.println(p);
+
+        Circle c = new Circle(p,4);
+        System.out.println(c.calcArea());
+    }
+}
